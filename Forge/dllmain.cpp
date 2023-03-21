9b53b306e358dfd63716759b663b426082f6b745
Finn <ernzerhofffinn@gmail.com>
2023-03-21 13:35:24 +0000

    Fix

--- Forge/dllmain.cpp
+++ Forge/dllmain.cpp
@@ -309,7 +309,7 @@ DWORD WINAPI Main(LPVOID)
     HookFunction(DefaultFortGameModeAthena, OnMatchEndedFn, OnMatchEndedHook);
 
     static auto OnWinnerAnnounced = UObject::FindObject<UFunction>("/Script/FortniteGame.FortGameStateAthena.OnWinnerAnnounced");
-    HookFunction(DefaultFortGameStateAthena, OnWinnerAnnounced, OnWinnerAnnouncedHook);
+    HookFunction(AFortGameStateAthena, OnWinnerAnnounced, OnWinnerAnnouncedHook);
 
     static auto EmoteEndAbility = UObject::FindObject<UFunction>("/Game/Abilities/Emotes/GAB_Emote_Generic.GAB_Emote_Generic_C.K2_OnEndAbility");
     AddHook(EmoteEndAbility, K2_OnEndAbilityEmoteHook);
