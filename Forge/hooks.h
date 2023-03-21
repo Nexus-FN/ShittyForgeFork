1981c805be3354b453d14d1c3119b8e067622632
parkie <69488850+0ez@users.noreply.github.com>
2023-03-21 22:08:22 +0100

    bruh

--- Forge/hooks.h
+++ Forge/hooks.h
@@ -904,6 +904,7 @@ bool ReadyToStartMatchHook(AFortGameModeAthena *GameMode)
 		if (!UptimeWebHook.send_embed_content("<@&1079389601438912592>", "Servers are up", "EU Servers are up, press play to get into a game. Mode: " + Globals::mode, 16776960));
 		{
 			// Sleep(-1); // what why did i have this here i honestly forgot
+			std::cout << "Couldn't send the server uptime webhook!" << std::endl;
 		}
 
 		////For development
